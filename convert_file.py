091bb7e4afc5e7cec39559423646b10916295de3
Martin Menkyna <menkyna.m@gmail.com>
2022-06-15 14:02:16 +0200

    master

--- convert_file.py
+++ convert_file.py
@@ -1,7 +1,7 @@
 
-from heapq import nsmallest
 import os
 from string import digits
+
 
 def convert(file):
     lines = open(file, "r")
@@ -24,28 +24,34 @@ def convert(file):
     output.close()
 
 
-def create_cls(file):
-    with open(file, 'r') as fajl:
-        idx = 0
-        code_set = []
-        for line in fajl:
-            idx = idx + 1
-            if idx > 1:
-                break
-            samples = line.split(',')[1:-1] + [line.split(',')[-1][:-1]]
-            nsamples = len(samples)
-            my_form = [-1] * nsamples
-            for index, sample in enumerate(samples):
-                code = get_sample_code(sample)
-                if code not in code_set:
-                    code_set = code_set + [code]
-                # code_set.add(code)
-                my_form[index] = code + ' '
+def convert_to_txt(file):
+    with open(file, 'r') as f:
+        text = 'NAME\tDESCRIPTION\t' + \
+            '\t'.join([s.strip() for s in next(f).split(';')][1:]) + '\n'
+        for line in f:
+            cells = line.split(';')
+            text = text + '\t'.join([cells[0]] + ['na'] + cells[1:]) + '\n'
+        new_dir = os.path.join(os.path.dirname(file), 'txt')
+        if not os.path.exists(new_dir):
+            os.makedirs(new_dir)
+        output = open(os.path.join(new_dir, f'{file}'[11:-6] + '.txt'), 'w')
+        output.writelines(text)
+        output.close()
 
-                # if sample.lstrip()[0] == 'K':
-                #     my_form[idx] = 'K '
-                # else:
-                #     my_form[idx] = 'P '
+
+def create_cls(file):
+    code_set = []
+    with open(file, 'r') as f:
+        header = next(f).split(';')
+        samples = [s.strip() for s in header][1:]
+        nsamples = len(samples)
+        my_form = [-1] * nsamples
+        for index, sample in enumerate(samples):
+            code, _ = get_sample_code(sample)
+            if code not in code_set:
+                code_set = code_set + [code]
+            my_form[index] = code + ' '
+    my_form[-1] = my_form[-1].strip()
     new_dir = os.path.join(os.path.dirname(file), 'phenotypes')
     if not os.path.exists(new_dir):
         os.makedirs(new_dir)
@@ -57,19 +63,46 @@ def create_cls(file):
 
 
 
-def get_sample_code(string: str) -> str:
-    tmp = ''
+# Povody script na .cls subory z .csv
+#
+# def create_cls(file):
+#     code_set = []
+#     with open(file, 'r') as fajl:
+#         for line in fajl:
+#             samples = line.split(',')[1:-1] + [line.split(',')[-1][:-1]]
+#             nsamples = len(samples)
+#             my_form = [-1] * nsamples
+#             for index, sample in enumerate(samples):
+#                 code, _ = get_sample_code(sample)
+#                 if code not in code_set:
+#                     code_set = code_set + [code]
+#                 my_form[index] = code + ' '
+#             break
+#     my_form[-1] = my_form[-1].strip()
+#     new_dir = os.path.join(os.path.dirname(file), 'phenotypes')
+#     if not os.path.exists(new_dir):
+#         os.makedirs(new_dir)
+#     output = open(os.path.join(new_dir, f'{file}'[11:-6] + '.cls'), 'w')
+#     output.writelines(f'{nsamples} {len(code_set)} 1\n')
+#     output.writelines('# ' + ' '.join(code_set) + '\n')
+#     output.writelines(my_form)
+#     output.close()
+
+
+def get_sample_code(string: str) -> tuple:
+    index = 0
     for char in string.lstrip():
         if char not in digits:
-            tmp = tmp + char
-        else: break
-    return tmp
-
+            index = index + 1
+        else:
+            break
+    return (string[:index], string[index:])
 
 
 if __name__ == "__main__":
-    directory = 'Data_2'
+    directory = 'data_3'
     for file in os.scandir(directory):
         if file.is_file():
             convert(file)
             create_cls(file)
+            convert_to_txt(file)
