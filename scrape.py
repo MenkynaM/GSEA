6c1841789d453c8941f1ac2e77c3b8f61cf22b5f
Martin Menkyna <menkyna.m@gmail.com>
2022-06-20 22:28:27 +0200

    updating scrape

--- scrape.py
+++ scrape.py
@@ -1,12 +1,20 @@
+import os
 from bs4 import BeautifulSoup
 import csv
 
-contents = open('data/gsea/REACTOME_FORMATION_OF_THE_CORNIFIED_ENVELOPE.html', 'r').read()
+contents = open(os.path.join(os.getcwd(), 'data', 'gsea', 'REACTOME_FORMATION_OF_THE_CORNIFIED_ENVELOPE.html'), 'r').read()
 soup = BeautifulSoup(contents, 'html.parser')
 
 
 div = soup.select('.richTable')[0]
 head = [col.text for col in div.select('th')[1:]]
-print(head)
-for row in div.find_all('tr'):
-    print([col.text for col in row.select('td')[1:]])
+with open('test.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(head)
+    for row in div.select('tr'):
+        writer.writerow([col.text for col in row.select('td')[1:]])
+
+# print(head)
+# print(len(list(div.find_all('tr'))))
+# for row in div.find_all('tr'):
+    # print([col.text for col in row.select('td')[1:]])
