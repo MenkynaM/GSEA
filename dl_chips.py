ddb6dee52394decd2816db351843224c441e1f34
Martin Menkyna <menkyna.m@gmail.com>
2022-07-07 19:18:04 +0200

    finished script to download all of .chip files

--- dl_chips.py
+++ dl_chips.py
@@ -6,17 +6,46 @@ def get_file_list(url: str) -> list:
     '''Opens `https://data.broadinstitute.org/gsea-msigdb/msigdb/annotations_versioned/`
     to search for all Human_*.chip files and returns their list
     '''
-    url = requests.get(url).text
-    soup = BeautifulSoup(url, 'html.parser')
+
+    # open `url` and search for all `<a>` elements
+    url_new = requests.get(url).text
+    soup = BeautifulSoup(url_new, 'html.parser')
     tmp = soup.select('a')
-    a = []
-    for elem in tmp:
-        if elem.text.startswith('H'):
-            a.append('https://data.broadinstitute.org/gsea-msigdb/msigdb/annotations_versioned/' + elem.attrs['href'])
+
+    # get their link attributes and return in the form of a list
+    a = [elem.attrs['href'] for elem in tmp if elem.text.startswith('H')]
     return a
+
+
+def download_files(lst: list, url: str) -> None:
+    '''Download the files listed in `lst` from `url`
+    '''
+
+    # create new directory, if nonexistent, and change there
+    new_dir = os.path.join(os.getcwd(), 'data', 'chips')
+    if not os.path.exists(new_dir):
+        os.makedirs(new_dir)
+    os.chdir(new_dir)
+
+    # download all `*.chip` files from `lst` in `url`
+
+    # WINDOWS
+    if os.name == 'nt':
+        for item in lst:
+            os.system('curl -o ' + item + ' ' + url + item)
+
+    # LINUX
+    if os.name == 'posix':
+        for item in lst:
+            os.system('wget ' + url + item)
+    
+    # change the directory back
+    os.chdir('../..')
 
 
 
 
 if __name__ == '__main__':
-    print(get_file_list('https://data.broadinstitute.org/gsea-msigdb/msigdb/annotations_versioned/'))
+    url = 'https://data.broadinstitute.org/gsea-msigdb/msigdb/annotations_versioned/'
+    lst = get_file_list(url)
+    download_files(lst, url)
